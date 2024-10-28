d15f7dd14f3a4eaed0579ecd71f2ac12648dc475
maicow259 <143803634+maicow259@users.noreply.github.com>
2024-10-28 08:03:25 -0300

    Adicionado a condição de vitória e derrota

--- Script/game_scene.gd
+++ Script/game_scene.gd
@@ -3,6 +3,7 @@ extends Control
 var temtativa
 @onready var resposta: Label = $"Resposta margim/Resposta"
 var ramdow_number = 0
+@onready var imput_player: LineEdit = $"Imput Margim/Imput Player"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,12 +16,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-
-
 
 func _on_imput_player_text_submitted(new_text: String) -> void:
 	if temtativa > 0:
@@ -28,3 +25,19 @@ func _on_imput_player_text_submitted(new_text: String) -> void:
 		
 		if new_text == str(ramdow_number):
 			resposta.text = "Você Acertou!"
+
+func _on_guess_pressed() -> void:
+	if temtativa > 0:
+		temtativa -= 1
+		
+		if imput_player.text == str(ramdow_number):
+			resposta.text = "Você Acertou!"
+			
+		elif imput_player.text < str(ramdow_number):
+			resposta.text = "O numero é maior!"
+			
+		elif imput_player.text > str(ramdow_number):
+			resposta.text = "O numero é menor!"
+			
+		if temtativa <= 0:
+			print("Game Over!")
